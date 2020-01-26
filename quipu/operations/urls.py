5f993b2b10bc2f9d59823f1afe95eb9646e4ff0e
Marco Anton <mj4nt0n@gmail.com>
2020-01-26 09:05:52 +0100

    endpoint for simple calculator.

--- quipu/operations/urls.py
+++ quipu/operations/urls.py
@@ -6,5 +6,6 @@ app_name = 'operations'
 
 urlpatterns = [
     path('addition/', views.OperationView.as_view(operation_type="addition")),
-    path('multiplication/', views.OperationView.as_view(operation_type="multiplication"))
+    path('multiplication/', views.OperationView.as_view(operation_type="multiplication")),
+    path('simple-calculator/', views.SimpleCalculatorView.as_view())
 ]
--- quipu/operations/views.py
+++ quipu/operations/views.py
@@ -1,9 +1,12 @@
 from django.utils.translation import gettext as _
+from django.core.mail import mail_admins
 
 from rest_framework.exceptions import APIException
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+from calculator.simple import SimpleCalculator
 
 from .serializers import OperationSerializer
 
@@ -20,7 +23,7 @@ class OperationView(APIView):
 
         self.request.data.update({'a_type': self.operation_type})
         serializer = OperationSerializer(data=self.request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=False):
             result_data = {
                 "inputs": self.request.data,
                 "result": serializer.perform_operation()
@@ -33,3 +36,36 @@ class OperationView(APIView):
 
 class SimpleCalculatorView(APIView):
     """"""
+
+    def get(self, *args, **kwrgs):
+        """Using SimpleCalculator to get results of operations."""
+        calc_instance = SimpleCalculator()
+        expression = self.request.data.get('expression', None)
+        if not expression:
+            data = {"detail": _("'expression' is required.")}
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+        # TODO: abstract this to a method.
+        try:
+            calc_instance.run(expression)
+        except Exception as ex:  # TODO: better exception handling.
+            msg = f"api called with '{expression}'\nAnd got an '{ex}' while executing."
+            mail_admins('SimpleCalculator endpoint exception', msg)
+            return Response(
+                {"detail": _("Error with our logic while calculating your expression. "
+                             "Admins have been notified.")},
+                status=status.HTTP_400_BAD_REQUEST
+                )
+        else:
+            # TODO: better error handling from calc results
+            if type(calc_instance.lcd) == str and 'Error' in calc_instance.lcd:
+                error_data = {
+                    'detail': _(f"Unable to produce result. "
+                                f"Revise your expression: '{expression}' "
+                                f"Place space between your numbers. ")
+                    }
+                return Response(error_data, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                # in order to produce results, digits must be separated with spaces...
+                result_data = {'inputs': expression, 'result': calc_instance.lcd}
+                return Response(result_data, status=status.HTTP_200_OK)
